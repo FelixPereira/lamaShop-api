7751073b0d9f7df0139728240ab5b2da9342f279
Félix Pereira <felixpereira538@gmail.com>
2022-06-19 20:12:44 +0200

    Add JWT to secure our application

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // Register
 router.post("/register", async (req, res) => {
@@ -18,5 +19,39 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({username: req.body.username});
+    if(!user) {
+      return res.status(404).send("User does not exist.");
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password, 
+      process.env.DECODE_PASSWORD
+    );
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    if(originalPassword !== req.body.password) {
+      return res.status(401).send("Wrong credencial");
+    }
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id, 
+        isAdmin: user.isAdmin
+      }, 
+      process.env.JWT_SECRET_KEY,
+      {expiresIn: "3d"}
+    );
+
+    const { password, ...others } = user._doc;
+    res.send({ ...others, accessToken });
+  } catch(error) {
+      console.log(error.message);
+  }
+})
 
 module.exports = router;
